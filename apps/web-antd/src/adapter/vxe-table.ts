dbc2a662c90b594623e4e61d027c132861d0a3c1
lu-yanpeng <ly3566@qq.com>
2025-06-10 18:42:47 +0800

    perf: tsx转成h函数

--- apps/web-antd/src/adapter/vxe-table.ts
+++ apps/web-antd/src/adapter/vxe-table.ts
@@ -1,10 +1,12 @@
 import type { VxeTableGridOptions } from '@vben/plugins/vxe-table';
 
-import { h } from 'vue';
+import { h, shallowReadonly } from 'vue';
 
 import { setupVbenVxeTable, useVbenVxeGrid } from '@vben/plugins/vxe-table';
 
 import { Button, Image } from 'ant-design-vue';
+
+import DaisyuiWrap from '#/components/daisyui-wrap.vue';
 
 import { useVbenForm } from './form';
 
@@ -60,6 +62,72 @@ setupVbenVxeTable({
 
     // 这里可以自行扩展 vxe-table 的全局配置，比如自定义格式化
     // vxeUI.formats.add
+    // 自定义复选框，配置columns.cellRender就可以使用这个组件
+    // columns: [ { cellRender: { name: 'checkbox' }} ]
+    vxeUI.renderer.add('checkbox', {
+      renderTableDefault({ props }, params) {
+        // 定义columns的时候可以传递props，disabled是否禁用复选框
+        const { disabled = true } = props ?? {
+          disabled: true,
+        };
+        const { row, column } = params;
+        const { field } = column;
+        const checked = Boolean(row[field]);
+        return h('div', null, [
+          h('div', { class: 'flex justify-center' }, [
+            h('input', {
+              type: 'checkbox',
+              checked,
+              disabled,
+              class: 'd-checkbox d-checkbox-sm d-checkbox-info',
+            }),
+          ]),
+        ]);
+      },
+    });
+
+    // 把这个单元格变成可以点击的链接，会触发click事件
+    vxeUI.renderer.add('link', {
+      renderTableDefault({ events }, params) {
+        const { row, column } = params;
+        const { field } = column;
+        const text = row[field];
+
+        /*
+        * 在columns里面配置cellRender.events。这里就会调用events里面的事件
+        *
+        cellRender: {
+          events: {
+            click: (rowData, event) => {},
+          },
+        },
+        * */
+        const onClick = (e: Event) => {
+          if (events) {
+            // @ts-ignore: 编辑器没有正确识别events的类型，这里的click是columns.cellRender.events的函数
+            events?.click?.(shallowReadonly(row), e);
+          }
+        };
+        return h('div', null, [
+          h(
+            DaisyuiWrap,
+            { class: 'bg-transparent' },
+            {
+              default: () => [
+                h(
+                  'button',
+                  {
+                    class: 'd-btn d-btn-link d-btn-xs',
+                    onClick,
+                  },
+                  text,
+                ),
+              ],
+            },
+          ),
+        ]);
+      },
+    });
   },
   useVbenForm,
 });
